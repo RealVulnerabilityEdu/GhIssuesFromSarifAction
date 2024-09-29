bc9373df3adb7ca9e57c862e15aa79d8a5dd53d2
Hui Chen <chen@sci.brooklyn.cuny.edu>
2024-09-29 03:26:30 +0000

    downloaded toolkit

--- init/download_toolkit.sh
+++ init/download_toolkit.sh
@@ -48,7 +48,7 @@ gh_issues.sh
 EOT
 
 FILE_LIST=$(ls "${INPUTS_TOOLKIT_DIRECTORY}")
-echo "file_list=$FILE_LIST" >>"$GITHUB_OUTPUT"
-echo "time=$(date)" >>"$GITHUB_OUTPUT"
+echo "file_list=\"$FILE_LIST\"" >> "$GITHUB_OUTPUT"
+echo "time=$(date)" >> "$GITHUB_OUTPUT"
 SHORT_FILE_LIST=$(ls "${INPUTS_TOOLKIT_DIRECTORY}")
 echo "::notice file=download_toolkit.sh,line=50::Downloaded the toolkit files ${SHORT_FILE_LIST} to ${INPUTS_TOOLKIT_DIRECTORY}"
