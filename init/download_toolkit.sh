366ceac2dc3a9b17ba022d4f63a0c414f4d04303
Hui Chen <chen@sci.brooklyn.cuny.edu>
2024-09-29 16:52:25 +0000

    attempted to fix a path problem

--- init/download_toolkit.sh
+++ init/download_toolkit.sh
@@ -29,17 +29,18 @@ if ! command -v wget >/dev/null 2>&1; then
 fi
 
 # Create the toolkit directory
-if ! mkdir -p "$INPUTS_TOOLKIT_DIRECTORY"; then
-	echo "::error file=download_toolkit.sh,line=33::Failed to create the toolkit directory"
+TOOKKIT_DIRECTORY="${HOME}/${INPUTS_TOOLKIT_DIRECTORY}"
+if ! mkdir -p "$TOOKKIT_DIRECTORY"; then
+	echo "::error file=download_toolkit.sh,line=34::Failed to create the toolkit directory"
 	exit 1
 fi
 
 while read -r FILE; do
 	FILENAME=$(basename "${FILE}")
 	SOURCE_FILE="${INPUTS_TOOLKIT_URL}/${FILENAME}"
-	TOOLKIT_FILE="${INPUTS_TOOLKIT_DIRECTORY}/${FILENAME}"
+	TOOLKIT_FILE="${TOOKKIT_DIRECTORY}/${FILENAME}"
 	if ! wget "${SOURCE_FILE}" -O "${TOOLKIT_FILE}"; then
-		echo "::error file=download_toolkit.sh,line=39::Failed to download $SOURCE_FILE"
+		echo "::error file=download_toolkit.sh,line=42::Failed to download $SOURCE_FILE"
 		exit 1
 	fi
 done <<'EOT'
@@ -49,14 +50,14 @@ parse_sarif.sh
 EOT
 
 FILE_LIST=""
-for FILE in "${INPUTS_TOOLKIT_DIRECTORY}"/*; do
+for FILE in "${TOOKKIT_DIRECTORY}"/*; do
 	FILE_LIST="${FILE_LIST}<$FILE> "
 done
 
 {
-	echo "toolkit_path=${INPUTS_TOOLKIT_DIRECTORY}"
+	echo "toolkit_path=${TOOKKIT_DIRECTORY}"
 	echo "file_list=${FILE_LIST}"
 	echo "time=$(date)"
 } >>"$GITHUB_OUTPUT"
 
-echo "::notice file=download_toolkit.sh,line=62::Downloaded toolkit files ${FILE_LIST} to ${INPUTS_TOOLKIT_DIRECTORY}"
+echo "::notice file=download_toolkit.sh,line=63::Downloaded toolkit files ${FILE_LIST} to ${TOOKKIT_DIRECTORY}"
