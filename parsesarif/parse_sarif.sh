de0a68aa193a2311b7901b0abc104adc8465e07d
Hui Chen <chen@sci.brooklyn.cuny.edu>
2024-09-30 03:37:26 +0000

    fixing linting errors

--- parsesarif/parse_sarif.sh
+++ parsesarif/parse_sarif.sh
@@ -1,8 +1,8 @@
 #!/bin/sh -l
 
 if [ $# -ne 6 ]; then
-    echo "Usage: $0 <repository> <sha> <sarif-data-path> <toolkit-path> <qhelp-root> <issue-data-path>"
-    exit 1
+	echo "Usage: $0 <repository> <sha> <sarif-data-path> <toolkit-path> <qhelp-root> <issue-data-path>"
+	exit 1
 fi
 
 REPOSITORY=$1
@@ -12,16 +12,15 @@ TOOLKIT_PATH=$4
 QHELP_ROOT=$5
 ISSUE_DATA_PATH=$6
 
-
 if "${TOOLKIT_PATH}/parse_sarif.sh" \
-    "${REPOSITORY}" \
-    "${SHA}" \
-    "${SARIF_DATA_PATH}" \
-    "${TOOLKIT_PATH}" \
-    "${QHELP_ROOT}" \
-    "${ISSUE_DATA_PATH}"; then
-    ls -l "${ISSUE_DATA_PATH}"
-    echo "::set-output name=status::success"
+	"${REPOSITORY}" \
+	"${SHA}" \
+	"${SARIF_DATA_PATH}" \
+	"${TOOLKIT_PATH}" \
+	"${QHELP_ROOT}" \
+	"${ISSUE_DATA_PATH}"; then
+	ls -l "${ISSUE_DATA_PATH}"
+	echo "::set-output name=status::success"
 else
-    echo "::set-output name=status::failure"
+	echo "::set-output name=status::failure"
 fi
