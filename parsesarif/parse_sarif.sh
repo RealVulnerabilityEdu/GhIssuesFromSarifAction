440c99aa018db9e0494ac0f85d27c187326cecbd
Hui Chen <chen@sci.brooklyn.cuny.edu>
2024-09-30 03:32:46 +0000

    experimening with scripts

--- parsesarif/parse_sarif.sh
+++ parsesarif/parse_sarif.sh
@@ -1,13 +1,26 @@
 #!/bin/sh -l
 
-if "${{github.workspace}}/${{inputs.toolkit-path}}/parse_sarif.sh" \
-    "${{github.repository}}" \
-    "${{github.sha}}" \
-    "${{inputs.sarif-data-path}}" \
-    "${{inputs.toolkit-path}}" \
-    "${{inputs.qhelp-root}}" \
-    "${{inputs.issue-data-path}}"; then
-    ls -l "${{inputs.issue-data-path}}"
+if [ $# -ne 6 ]; then
+    echo "Usage: $0 <repository> <sha> <sarif-data-path> <toolkit-path> <qhelp-root> <issue-data-path>"
+    exit 1
+fi
+
+REPOSITORY=$1
+SHA=$2
+SARIF_DATA_PATH=$3
+TOOLKIT_PATH=$4
+QHELP_ROOT=$5
+ISSUE_DATA_PATH=$6
+
+
+if "${TOOLKIT_PATH}/parse_sarif.sh" \
+    "${REPOSITORY}" \
+    "${SHA}" \
+    "${SARIF_DATA_PATH}" \
+    "${TOOLKIT_PATH}" \
+    "${QHELP_ROOT}" \
+    "${ISSUE_DATA_PATH}"; then
+    ls -l "${ISSUE_DATA_PATH}"
     echo "::set-output name=status::success"
 else
     echo "::set-output name=status::failure"
