af63409dbde37abb73a9211392e503f7b631ef3a
Hui Chen <chen@sci.brooklyn.cuny.edu>
2024-09-30 22:33:31 +0000

    debugging toolkit

--- sariftoghissues/run_toolkit.sh
+++ sariftoghissues/run_toolkit.sh
@@ -94,6 +94,7 @@ fi
 if [ -n "${_SARIF2GHI_MOCKING_FOR_DEBUG_}" ]; then
 	echo "Mocking create issues due to _SARIF2GHI_MOCKING_FOR_DEBUG_=${_SARIF2GHI_MOCKING_FOR_DEBUG_}"
 fi
+ls -l "${TOOLKIT_PATH}"
 if "${TOOLKIT_PATH}/create_gh_issues.sh" \
 	"${INPUTS_ISSUE_DATA_PATH}"; then
 	{
